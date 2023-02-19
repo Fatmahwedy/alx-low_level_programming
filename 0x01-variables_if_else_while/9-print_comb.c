f9534efbb26cba394a7a22bb0a8cceadf4bca859
fatma <fatmahwedy505050@gamil.com>
2023-02-19 13:20:42 -0800

    $edit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdio.h>
+#include <time.h>
 
 /**
  * main - Prints all possible combinations of single-digit numbers.
@@ -9,17 +11,16 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
+		putchar((num);
+		if (num != '9')
+		{
 		
 		putchar(',');
 		putchar(' ');
+		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
