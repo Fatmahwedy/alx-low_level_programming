eac7fad36e7dd2e5d52c7562fc8c4306f85f2417
fatmahwedy <fatmahwedy505050@gmail.com>
2023-04-08 17:29:49 -0700

    desc

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation
+ * of a number.
+ * @n: unsigned long int.
+ *
+ * Return: no return.
+ */
+void print_binary(unsigned long int n)
---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given
+ * index.
+ * @n: unsigned long int input.
+ * @index: index of the bit.
+ *
+ * Return: value of the bit.
+ */
