13c79c91e03dc388a6aefb0777182ea5a541b0cc
fatmahwedy <fatmahwedy505050@gmail.com>
2023-04-10 19:51:50 -0700

    task2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,3 +8,27 @@
  *
  * Return: value of the bit.
  */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+    unsigned long int mask; /* declare mask */
+
+if (index >= sizeof(unsigned long int) * 8) /* check if index is out of bounds */
+{
+    return -1;
+}
+else
+{
+    mask = 1UL << index; /* set mask to 1 at the index-th bit */
+
+    if (n & mask) /* if the bit at index is 1 */
+    {
+        return 1;
+    }
+    else /* if the bit at index is 0 */
+    {
+        return 0;
+    }
+}
+}
+
