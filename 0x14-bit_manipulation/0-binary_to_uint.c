b48a1bad0daaf9af220b4a1880085fd893624f6d
fatmahwedy <fatmahwedy505050@gmail.com>
2023-04-10 19:15:08 -0700

    task0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,4 +8,21 @@
  * Return: unsigned int.
  */
 unsigned int binary_to_uint(const char *b)
+{ 
+    if (b == '\0') { 
+        return 0; 
+    } 
+
+    unsigned int result = 0; 
+
+    for (int i = 0; b[i] != '\0'; i++) { 
+        if (b[i] != '0' && b[i] != '1') { 
+            return 0; 
+        } 
+
+        result = result * 2 + (b[i] - '0'); 
+    } 
+
+    return result; 
+}
 
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,9 +8,7 @@
  * Return: no return.
  */
 void print_binary(unsigned long int n)
-	#include <stdio.h>
-
-void print_binary(unsigned long int n) {
+{
     unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
     while (mask > 0) {
         if (n & mask) {
