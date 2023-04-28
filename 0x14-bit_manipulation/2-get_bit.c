3497beed6f07165b2db5cd3b01cabbc6892b7881
fatmahwedy <fatmahwedy505050@gmail.com>
2023-04-28 10:16:40 -0700

    bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,27 +8,20 @@
  *
  * Return: value of the bit.
  */
-
 int get_bit(unsigned long int n, unsigned int index)
 {
-    unsigned long int mask; /* declare mask */
+	unsigned int i;
 
-if (index >= sizeof(unsigned long int) * 8) /* check if index is out of bounds */
-{
-    return -1;
-}
-else
-{
-    mask = 1UL << index; /* set mask to 1 at the index-th bit */
+	if (n == 0 && index < 64)
+		return (0);
 
-    if (n & mask) /* if the bit at index is 1 */
-    {
-        return 1;
-    }
-    else /* if the bit at index is 0 */
-    {
-        return 0;
-    }
-}
-}
+	for (i = 0; i <= 63; n >>= 1, i++)
+	{
+		if (index == i)
+		{
+			return (n & 1);
+		}
+	}
 
+	return (-1);
+}
