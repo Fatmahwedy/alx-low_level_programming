5c80672c9e2bdecf52975d24846660c80eef8ada
fatmahwedy <fatmahwedy505050@gmail.com>
2023-04-28 10:07:05 -0700

    read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,44 @@
 #include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints it to stdout
+ * read_textfile - reads a text file and prints it to the POSIX standard output
  * @filename: name of the file to read
- * @letters: number of letters to read and print
- * Return: the actual number of letters read and printed, or 0 on failure
+ * @letters: number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t num_read, num_written, total_written = 0;
-    char *buf = malloc(letters * sizeof(char));
-    int fd;
+	int fd;
+	ssize_t lenr, lenw;
+	char *buffer;
 
-    if (filename == NULL || buf == NULL)
-        return (0);
-
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
-
-    num_read = read(fd, buf, letters);
-    if (num_read == -1)
-    {
-        free(buf);
-        close(fd);
-        return (0);
-    }
-
-    num_written = write(STDOUT_FILENO, buf, num_read);
-    if (num_written != num_read)
-    {
-        free(buf);
-        close(fd);
-        return (0);
-    }
-
-    total_written += num_written;
-
-    free(buf);
-    close(fd);
-    return (total_written);
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	lenr = read(fd, buffer, letters);
+	close(fd);
+	if (lenr == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	lenw = write(STDOUT_FILENO, buffer, lenr);
+	free(buffer);
+	if (lenr != lenw)
+		return (0);
+	return (lenw);
 }
-
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,38 @@
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <string.h>
 #include "main.h"
 
 /**
- * create_file - creates a new file with the specified content.
- * @filename: the name of the file to create.
- * @text_content: the content of the file to create.
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
  *
- * Return: 1 if the file was successfully created, -1 otherwise.
+ * Return: 1 if it success. -1 if it fails.
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, ret = -1;
-    
-    if (filename == NULL)
-        return (-1);
-    
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    if (fd != -1)
-    {
-        if (text_content == NULL)
-            text_content = "";
-        ret = write(fd, text_content, strlen(text_content));
-        if (ret == -1)
-            ret = -1;
-        close(fd);
-    }
-    
-    return (ret == -1 ? -1 : 1);
-}
+	int fd;
+	int nletters;
+	int rwr;
 
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
